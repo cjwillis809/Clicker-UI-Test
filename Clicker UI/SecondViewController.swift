aeee69004baa4478b9f7b72e86d3316ee1cd5e40
CJ Willis <cjwillis809@gmail.com>
2020-05-22 16:06:58 -0500

    Initial Commit

---
+++ Clicker UI/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Clicker UI
+//
+//  Created by CJ Willis on 5/22/20.
+//  Copyright © 2020 CJ Willis. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
